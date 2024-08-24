5673eacc55da4978625aac25ceee3074ccb5f3b1
Game data <160002063+temcs@users.noreply.github.com>
2024-08-24 18:09:14 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -72,3 +72,21 @@ $(function(){
             });
         });
     });
+
+
+    // idenhi ivdi balvisi likki 10 arba 
+
+var clickCount = 0;
+var clickDiv = document.getElementById("clickDiv");
+var hiddenDiv = document.getElementById("hdn");
+
+if (!clickDiv || !hiddenDiv) {
+  console.error("Could not find clickDiv or hiddenDiv");
+} else {
+  clickDiv.addEventListener("click", function() {
+    clickCount++;
+    if (clickCount === 5) {
+      hiddenDiv.style.display = "block";
+    }
+  });
+}
