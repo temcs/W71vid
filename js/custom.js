ee739db76b68bd60a52ec95292f298611cc57425
Game data <160002063+temcs@users.noreply.github.com>
2024-08-24 21:23:14 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -74,6 +74,29 @@ $(function(){
     });
 
 
+    // flow shake 
+
+    function shakeChild(index) {
+        var flowChildren = document.querySelectorAll('.flow-child');
+        flowChildren.forEach(function(child, idx) {
+            if (idx === index) {
+                child.classList.add('shaking');
+            } else {
+                child.classList.remove('shaking');
+            }
+        });
+    }
+    
+    function shakeLoop() {
+        var index = 0;
+        setInterval(function() {
+            shakeChild(index);
+            index = (index + 1) % 3;
+        }, 3000); // Repeat the animation every 3 seconds
+    }
+    
+    shakeLoop();
+
     // idenhi ivdi balvisi likki 10 arba 
 
 var clickCount = 0;
